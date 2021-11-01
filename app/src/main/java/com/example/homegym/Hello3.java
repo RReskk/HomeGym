5f7dc6b3e290be4a39f7edc533a77b167edc7b29
RReskk <zyxorus@mail.ru>
2021-11-01 22:54:03 +0500

    Bug fixes

--- app/src/main/java/com/example/homegym/Hello3.java
+++ app/src/main/java/com/example/homegym/Hello3.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Hello3 extends AppCompatActivity {
 
-    ImageView image;
+    ImageView man_image;
+    ImageView woman_image;
     SharedPreferences pref;
     String gender;
     private final String genderPref = "gender";
@@ -18,19 +21,19 @@ public class Hello3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello3);
         Init();
+
+        if (gender.equals("male")) {
+            man_image.setVisibility(View.VISIBLE);
+        }
+        else if (gender.equals("female")) {
+            woman_image.setVisibility(View.VISIBLE);
+        }
     }
 
     private void Init() {
-//        пока что не работает
-//        image = findViewById(R.id.shadow);
-//        pref = getSharedPreferences("Values", MODE_PRIVATE);
-//        gender = pref.getString(genderPref, "null");
-//
-//        if (gender.equals("male")) {
-//            image.setBackgroundResource(R.drawable.male);
-//        }
-//        else {
-//            image.setBackgroundResource(R.drawable.female);
-//        }
+        man_image = findViewById(R.id.man_shadow);
+        woman_image = findViewById(R.id.woman_shadow);
+        pref = getSharedPreferences("Values", MODE_PRIVATE);
+        gender = pref.getString(genderPref, "");
     }
 }
